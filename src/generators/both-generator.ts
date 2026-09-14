4fd5c328aec677fa7c8c11203f02342be11ae7c1
Usama Imran <uusamaimran@gmail.com>
2026-09-14 16:53:28 +0500

    Add combined generator for template selection

---
+++ src/generators/both-generator.ts
@@ -0,0 +1,7 @@
+import { generatePDF } from "../utils/pdf-generator";
+import { GENERATOR_CONFIG } from "./generators.config";
+
+(async () => {
+  await generatePDF(GENERATOR_CONFIG.RESUME);
+  await generatePDF(GENERATOR_CONFIG.COVER_LETTER);
+})();
